493d33d9164cb3e27edb00260b68a28abc460438
Joshua Wise <joshua@joshuawise.com>
2022-07-16 04:57:18 -0400

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='tsheetspy',
+    version='0.1.0',
+    description='Python T-Sheets command line interface',
+    author='Joshua Wise',
+    packages=['tsheetspy'],
+    scripts=['tsheetspy/scripts/tsheets'],
+    install_requires=[
+    ]
+)
